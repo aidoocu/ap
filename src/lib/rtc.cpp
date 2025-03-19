0ad18f2eac1879414ed129d697eb95ff153dd709
Bernardo Yaser León Ávila <bernardoyla@gmail.com>
2025-03-19 09:19:50 -0400

    set date_time funtion

--- src/lib/rtc.cpp
+++ src/lib/rtc.cpp
@@ -8,12 +8,43 @@
 
 uRTCLib rtc(RTC_ADDR);
 
-void time(char * time) {
+
+void get_time(char * time) {
 
 	rtc.refresh();
 
     /* dd/mm/aa,hh:mm:ss */
 	sprintf(time, "%02d/%02d/%02d,%02d:%02d:%02d", rtc.day(), rtc.month(), rtc.year(), rtc.hour(), rtc.minute(), rtc.second());
 	
+
+}
+
+void set_date_time(char * date_time) {
+
+	/** RTCLib::set(byte second, 
+	 * byte minute, 
+	 * byte hour (0-23:24-hr mode only),
+	 * byte dayOfWeek (Sun = 1, Sat = 7), 
+	 * byte dayOfMonth (1-12), 
+	 * byte month, 
+	 * byte year) */
+
+	//Serial.println(date_time);
+	//delay(20);
+
+ 	uint8_t second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+
+	// Parsear la cadena
+	sscanf(date_time, "%hhu,%hhu,%hhu,%hhu,%hhu,%hhu,%hhu", 
+			&second, 
+			&minute, 
+			&hour, 
+			&dayOfWeek, 
+			&dayOfMonth, 
+			&month, 
+			&year);
+   
+	// Configurar RTC
+	rtc.set(second, minute, hour, dayOfWeek, dayOfMonth, month, year);
 
 }
--- src/lib/rtc.h
+++ src/lib/rtc.h
@@ -19,14 +19,14 @@
 #endif /* RTC_DEV == RTC_DEV_DEFAULT */
 
 
-
-
 /** Esta funcion escribe la cadena dd/mm/aa,hh:mm:ss
 *  a partir time. Esta funcion NO chequea el largo 
 *  del buffer, por lo que al ser llamada hay que asegurarse
 *  que el buffer apuntado por time sea no menos que 
 *  DATE_TIME_BUFF 18
 */
-void time(char * time);
+void get_time(char * time);
 
 #endif /* _RTC_H_ */
+
+void set_date_time(char * date_time);
